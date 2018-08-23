7a6ea161b4e3c999c427f2a56e15464b6a27fedc
wanghongkun <wanghongkun@corp.netease.com>
2018-08-23 11:43:52 +0800

    导入数据库

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -7,72 +7,22 @@
 
 <script>
 /* eslint-disable */
-import Cascader from "@components/cascader/cascader";
+import Cascader from '@components/cascader/cascader'
+import db from './db'
+
+function ajax(parentId = 0) {
+  return db.filter((item) => item.parent_id == parentId)
+}
+console.log(ajax())
+
 export default {
-  name: "AssetTask",
+  name: 'AssetTask',
   components: {
     Cascader
   },
   data() {
     return {
-      source: [
-        {
-          name: "浙江",
-          children: [
-            {
-              name: "杭州",
-              children: [
-                { name: "萧山区" },
-                { name: "古荡区" },
-                { name: "西湖区" },
-                { name: "滨江区" }
-              ]
-            },
-            {
-              name: "宁波",
-              children: [
-                { name: "江东区" },
-                { name: "江北区" },
-                { name: "北仓区" }
-              ]
-            },
-            {
-              name: "温州",
-              children: [
-                { name: "市辖区" },
-                { name: "瑞安区" },
-                { name: "龙湾区" }
-              ]
-            }
-          ]
-        },
-        {
-          name: "山东",
-          children: [
-            {
-              name: "济南",
-              children: [
-                { name: "历下区" },
-                { name: "历城区" },
-                { name: "高新区" },
-                { name: "天桥区" }
-              ]
-            },
-            {
-              name: "青岛",
-              children: [
-                { name: "市南区" },
-                { name: "黄岛区" },
-                { name: "四方区" }
-              ]
-            }
-          ]
-        },
-        {
-          name: "北京",
-          children: [{ name: "五道口" }, { name: "大兴" }]
-        }
-      ],
+      source: ajax(),
       selected: []
     }
   }
@@ -81,7 +31,8 @@ export default {
 
 <style lang="scss" scoped>
 .main-content {
-  text-align: center; padding-top: 40px;
+  text-align: center;
+  padding-top: 40px;
   .cascader {
     display: inline-block;
   }
